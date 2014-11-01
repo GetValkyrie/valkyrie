2b7c49f0c6001c41deb8aad4267e6ff5b8135638
Christopher Gervais <chris@ergonlogic.com>
2014-11-01 19:14:12 -0400

    Fix Avahi service provisioning.

--- modules/avahi/manifests/init.pp
+++ modules/avahi/manifests/init.pp
@@ -6,19 +6,23 @@ class avahi inherits aegir::defaults {
     before => Supervisor::Service['avahi-aliases'],
   }
 
-  include supervisor
-  supervisor::service { 'avahi-aliases':
-    ensure  => 'running',
-    user    => 'aegir',
-    command => '/var/aegir/.drush/provision_avahi/publish.py --directory=/var/aegir/config/avahi-aliases',
-  }
-
   # Install Provision extension.
   drush::git {'provision_avahi':
     path     => '/var/aegir/.drush/',
-    git_repo => 'http://git.poeticsystems.com:valkyrie/provision_avahi.git',
+    git_repo => 'git@git.poeticsystems.com:valkyrie/provision_avahi.git',
     user     => 'aegir',
     require  => $aegir_installed,
+  }
+
+  file {'/etc/avahi/aliases.d':
+    ensure => directory,
+  }
+
+  include supervisor
+  supervisor::service { 'avahi-aliases':
+    ensure  => 'running',
+    command => '/var/aegir/.drush/provision_avahi/publish.py --directory=/var/aegir/config/avahi-aliases',
+    require => Drush::Git['provision_avahi'],
   }
 
   # Allow 'aegir' user to restart the avahi-aliases daemon.
