20c5e7d65ccf642302a493c96b0b25a0b1c48e0c
Christopher Gervais <chris@ergonlogic.com>
2014-11-17 14:46:25 -0500

    Cleanup vagrant-dns config.

--- vagrant/lib/vagrant-dns.rb
+++ vagrant/lib/vagrant-dns.rb
@@ -1,25 +1,60 @@
-name = File.basename(File.dirname(__FILE__))
-config.vm.hostname = "#{name}.val"
-# Setup DNS server to resolve *.val
-config.dns.tld = 'val'
-config.dns.patterns = [/^.*\.val$/]
+# Configures Vagrant to use the vagrant-dns plugin based on Valkyrie project
+# settings.
+# Params:
+# +config+:: The Vagrant config object.
+# +conf+:: A Valkyrie project configuration array.
+def configure_vagrant_dns(config, conf)
 
-config.trigger.after [:up, :reload, :resume] do
-  puts 'Installing DNS resolver...'
-  system 'vagrant dns --install'
-  system 'vagrant dns --start'
+  # Set hostname
+  config.vm.hostname = "#{conf['name']}.#{conf['tld']}"
+
+  # Setup DNS server to resolve our TLD
+  config.dns.tld = conf['tld']
+  config.dns.patterns = ["/^.*\.#{conf['tld']}$/"]
+
+  config.trigger.after [:up, :reload, :resume] do
+    puts 'Installing DNS resolver...'
+    system 'vagrant dns --install'
+    system 'vagrant dns --start'
+  end
+
+  config.trigger.before [:halt, :suspend] do
+    # Stop DNS server
+    system 'vagrant dns --stop'
+  end
+
+  config.trigger.after [:destroy] do
+    # Remove vagrant-dns TLDs
+    puts 'Removing DNS resolver files...'
+    system 'vagrant dns --purge'
+    system 'vagrant dns --stop'
+  end
+
 end
 
-config.trigger.before [:halt, :suspend] do
-  # Stop DNS server
-  system 'vagrant dns --stop'
+# Document steps to install vagrant-dns plugin.
+# Params:
+# +config+:: The Vagrant config object.
+# +conf+:: A Valkyrie project configuration array.
+def install_vagrant_dns(config, conf)
+  if (/darwin/ =~ RUBY_PLATFORM) != nil and conf['use_vagrant_dns']
+    puts 'The vagrant-dns plugin does not appear to be installed.'
+    puts 'To install it, run the following command:'
+    puts
+    puts '    vagrant plugin install vagrant-dns'
+    puts
+    puts 'This error can be suppressed by adding the following line to config.yml at'
+    puts 'the root of your project:'
+    puts
+    puts 'use_vagrant_dns: false'
+    abort()
+  end
 end
 
-config.trigger.after [:destroy] do
-  # Remove vagrant-dns TLDs
-  puts 'Removing DNS resolver files...'
-  system 'vagrant dns --purge'
-  system 'vagrant dns --stop'
+# Fallback to Avahi aliases generated and announced by Aegir in the VM.
+# Params:
+# +config+:: The Vagrant config object.
+# +conf+:: A Valkyrie project configuration array.
+def avahi_fallback(config, conf)
+  config.vm.hostname = conf['name']+'.local'
 end
-
-
