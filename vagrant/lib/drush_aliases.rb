cf857b7a2c3582570670a539695f06a620458448
Christopher Gervais <chris@ergonlogic.com>
2014-11-17 16:24:09 -0500

    Fix syntax.

--- vagrant/lib/drush_aliases.rb
+++ vagrant/lib/drush_aliases.rb
@@ -1,9 +1,9 @@
-project_root = ENV['project_root']
-valkyrie_root = ENV['valkyrie_root']
-aliases_path = "#{project_root}/.valkyrie/cache/aliases"
-
 # Register our project as a place for Drush to find aliases.
 def drush_aliases(config)
+  project_root = ENV['project_root']
+  valkyrie_root = ENV['valkyrie_root']
+  aliases_path = "#{project_root}/.valkyrie/cache/aliases"
+
   config.trigger.before [:up, :reload, :resume] do
     # Set up drush aliases
     system "drush #{valkyrie_root}/lib/bin/set_alias_path.php #{aliases_path}"
