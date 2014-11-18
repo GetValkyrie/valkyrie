930b651ffc8535d4c21a800e08a2653e883204c9
Christopher Gervais <chris@ergonlogic.com>
2014-11-18 12:20:33 -0500

    Remove vagrant-sshfs plugin and legacy plugin-handling code.

--- vagrant/lib/plugins/vagrant-sshfs/lib/vagrant-sshfs/errors.rb
+++
@@ -1,7 +0,0 @@
-module Vagrant
-  module SshFS
-    class Error < Errors::VagrantError
-      error_namespace("vagrant.config.sshfs.error")
-    end
-  end
-end
--- vagrant/lib/plugins/vagrant-sshfs/lib/vagrant-sshfs/version.rb
+++
@@ -1,5 +0,0 @@
-module Vagrant
-  module SshFS
-    VERSION = "0.0.6.qnd2"
-  end
-end
