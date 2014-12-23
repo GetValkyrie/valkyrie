00273c870e2cf99fceff4d2dfeab67977e047441
Christopher Gervais <chris@ergonlogic.com>
2014-12-23 11:34:55 -0500

    Don't rely on env var when creating aliases.

--- vagrant/lib/drush_aliases.rb
+++ vagrant/lib/drush_aliases.rb
@@ -1,17 +1,15 @@
 # Register our project as a place for Drush to find aliases.
-def drush_aliases(config)
-  project_root = ENV['project_root']
-  valkyrie_root = ENV['valkyrie_root']
+def drush_aliases(config, project_root)
   aliases_path = "#{project_root}/.valkyrie/cache/aliases"
 
   config.trigger.after [:up, :reload, :resume] do
     # Set up drush aliases
-    system "drush #{valkyrie_root}/lib/bin/set_alias_path.php #{aliases_path}"
+    system "drush #{project_root}/.valkyrie/valkyrie/vagrant/lib/bin/set_alias_path.php #{aliases_path}"
   end
 
   config.trigger.after [:destroy] do
     # Remove drush aliases
-    system "drush #{valkyrie_root}/lib/bin/unset_alias_path.php #{aliases_path}"
+    system "drush #{project_root}/.valkyrie/valkyrie/vagrant/lib/bin/unset_alias_path.php #{aliases_path}"
   end
 
 end
