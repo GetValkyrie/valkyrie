4dc3341f2bbbb19da33de2658b89d5f6b34fbe1f
Christopher Gervais <chris@ergonlogic.com>
2015-01-09 11:27:16 -0500

    Flatten directory structure a bit.

--- vagrant/drush_aliases.rb
+++ vagrant/drush_aliases.rb
@@ -4,12 +4,12 @@ def drush_aliases(config, project_root)
 
   config.trigger.after [:up, :reload, :resume] do
     # Set up drush aliases
-    system "drush #{project_root}/.valkyrie/valkyrie/vagrant/lib/bin/set_alias_path.php #{aliases_path}"
+    system "drush #{project_root}/.valkyrie/valkyrie/vagrant/bin/set_alias_path.php #{aliases_path}"
   end
 
   config.trigger.after [:destroy] do
     # Remove drush aliases
-    system "drush #{project_root}/.valkyrie/valkyrie/vagrant/lib/bin/unset_alias_path.php #{aliases_path}"
+    system "drush #{project_root}/.valkyrie/valkyrie/vagrant/bin/unset_alias_path.php #{aliases_path}"
   end
 
 end
--- vagrant/load_config.rb
+++ vagrant/load_config.rb
@@ -5,7 +5,7 @@ def load_config
   valkyrie_root = ENV['valkyrie_root']
 
   # Load default config
-  conf = YAML.load_file("#{valkyrie_root}/lib/config.yaml")
+  conf = YAML.load_file("#{valkyrie_root}/config.yaml")
   # Merge in project-specific settings
   if File.exist?("#{project_root}/.valkyrie/config.yaml")
     conf.merge!(YAML.load_file("#{project_root}/.valkyrie/config.yaml"))
