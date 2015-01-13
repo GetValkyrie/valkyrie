774ff2bb76608d2b63c8f0fd8472d9326337f670
Christopher Gervais <chris@ergonlogic.com>
2015-01-13 13:56:16 -0500

    Remove 'remote_host' option from aliases.
    Since setting a 'remote-host' option would cause commands to be run on the
    remote, we pass in an option that won't affect operations: 'remote_host'.
    We then translate that back to 'remote-host' when we're creating the alias.
    However, we don't need (or want) the meaningless 'remote_host' entry, so
    we remove it.

--- Provision/Context/Valkyrie.php
+++ Provision/Context/Valkyrie.php
@@ -8,17 +8,18 @@ class Provision_Context_Valkyrie extends Provision_Context {
 
   function init_Valkyrie() {
     if ($options = drush_get_option_list('options', FALSE)) {
+      if ($index = array_search('remote_host', $options) && $remote_host = drush_get_option('remote_host', FALSE)) {
+        // Setting a 'remote-host' option would cause commands to be run on the
+        // remote. So we pass in an option that won't affect operations, in order
+        // to set it in the context here as 'remote-host'.
+        $this->setProperty('remote-host', $remote_host);
+        unset($options[$index]);
+      }
       foreach ($options as $option) {
         if (drush_get_option($option, FALSE)) {
           $this->setProperty($option);
         }
       }
-    }
-    // Setting a 'remote-host' option would cause commands to be run on the
-    // remote. So we pass in an option that won't affect operations, in order
-    // to set it in the context here as 'remote-host'.
-    if ($remote_host = drush_get_option('remote_host', FALSE)) {
-      $this->setProperty('remote-host', $remote_host);
     }
   }
 
